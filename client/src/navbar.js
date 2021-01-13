165fd9616aaac7509c08a38ea9e5836b0d97ca38
Sunny (Hui-Shan) Cheng <huishan.cheng@studio.unibo.it>
2021-01-13 15:21:17 -0500

    adding navbar css

--- client/src/navbar.js
+++ client/src/navbar.js
@@ -5,7 +5,7 @@ import Image from 'react-bootstrap/Image';
 
 export default function Nav() {
 
-  return(<nav class="navbar sticky-top navbar-dark bg-dark">
-  <Image src="https://allgoodshop.blob.core.windows.net/default/0000011_Logo-v3-44aa00-222x60.png" thumbnail />
+  return(<nav className="d-flex flex-row bg-white border-top border-bottom mt-5 ml-5 mr-5 navbar">
+  <Image className="mt-4 mb-4 border-0" src="https://allgoodshop.blob.core.windows.net/default/0000011_Logo-v3-44aa00-222x60.png" thumbnail />
 </nav>);
 }
